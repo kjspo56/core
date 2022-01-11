571e0a2aebb6c4932fc235b453ba94030aebd21c
JUNGSOO KIM <62382377+kjspo56@users.noreply.github.com>
2022-01-12 00:46:40 +0900

    Bean과 컨테이너 생성

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,20 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class MemberApp {
 
     public static void main(String[] args) { //psvm 누르고 치니깐 자동으로 완성
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+ //       AppConfig appConfig = new AppConfig();
+ //       MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);  //ctr + shift + v 단축키 사용하면 memberA 자동생성
         //id는 Long 타입이기때문에 1L로 붙여준거
         memberService.join(member);
