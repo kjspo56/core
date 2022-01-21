33eacadb8d1447ba247a27d55eb54b0349760eba
JUNGSOO KIM <62382377+kjspo56@users.noreply.github.com>
2022-01-21 23:06:28 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutoAppConfigTest {
+
     @Test
     void basicScan(){
         AnnotationConfigApplicationContext ac =  new AnnotationConfigApplicationContext(AutoAppConfig.class);
